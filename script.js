4010003c9397f5a2c69d22dabe0c9f2461556187
BenGavieres <benjamin.gavieres@gmail.com>
2023-07-12 18:06:13 -0400

    Simplified the code within the for-loop that creates the grid. Discovered GRID_WIDTH is unnecessary in calculating the number of total squares.
    Fixed a bug that created extra squares for certain values of inRow.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const gridContainer=document.querySelector(".grid-container");
-const GRID_PIXELS=window.getComputedStyle(gridContainer).getPropertyValue("width");
-GRID_WIDTH=parseInt(GRID_PIXELS,10);//removes "px" from end of GRID_PIXELS string
+//const GRID_PIXELS=window.getComputedStyle(gridContainer).getPropertyValue("width");
+//GRID_WIDTH=parseInt(GRID_PIXELS,10);//removes "px" from end of GRID_PIXELS string
+//grid width is 576 px
 console.log("grid width: "+GRID_WIDTH);//remove
 
-let dimension=8;//can be changed by user
-let squareWidth=Math.floor(GRID_WIDTH/dimension);
+let inRow=16;//can be changed by user
+let squareWidth=Math.floor(GRID_WIDTH/inRow);
 console.log("square width: "+squareWidth);//remove
 
 let color="blue"//can be changed by user;
 
-//creates grid
-for (let i=0;i<GRID_WIDTH*dimension;i+=squareWidth){
+for (let i=0;i<inRow**2;i++){
     const square=document.createElement("div");
     square.classList.add("square");
      
