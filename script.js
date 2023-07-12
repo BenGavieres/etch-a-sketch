5bab1e03306dc4b6efc3ddc2ba545271ed809ab9
BenGavieres <benjamin.gavieres@gmail.com>
2023-07-12 17:42:27 -0400

    Can change dimension to change grid

--- script.js
+++ script.js
@@ -7,8 +7,19 @@ let dimension=16;//can be changed by user
 let squareWidth=Math.floor(GRID_WIDTH/dimension);
 console.log("square width: "+squareWidth);//remove
 
+let color="blue"//can be changed by user;
+
+//creates grid
 for (let i=0;i<GRID_WIDTH*dimension;i+=squareWidth){
     const square=document.createElement("div");
     square.classList.add("square");
+     
+    square.style.width=`${squareWidth}.px`;
+    square.style.height=`${squareWidth}.px`;
+
     gridContainer.appendChild(square);
+
+    square.addEventListener("mouseover",square=>{
+        square.style.backgroundColor="blue";
+    });
 }
