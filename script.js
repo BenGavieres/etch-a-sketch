76880d080d8dc08f54cecedd05d13d0abeaa5fd9
BenGavieres <benjamin.gavieres@gmail.com>
2023-07-12 20:25:11 -0400

    Added color picker

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const gridContainer=document.querySelector(".grid-container");
-//const GRID_PIXELS=window.getComputedStyle(gridContainer).getPropertyValue("width");
-//GRID_WIDTH=parseInt(GRID_PIXELS,10);//removes "px" from end of GRID_PIXELS string
+const GRID_PIXELS=window.getComputedStyle(gridContainer).getPropertyValue("width");
+GRID_WIDTH=parseInt(GRID_PIXELS,10);//removes "px" from end of GRID_PIXELS string
 //grid width is 576 px
-console.log("grid width: "+GRID_WIDTH);//remove
 
 let inRow=16;//can be changed by user
 let squareWidth=Math.floor(GRID_WIDTH/inRow);
-console.log("square width: "+squareWidth);//remove
 
-let color="blue"//can be changed by user;
+let color="#000000"//can be changed by user;
 
 for (let i=0;i<inRow**2;i++){
     const square=document.createElement("div");
@@ -20,6 +18,18 @@ for (let i=0;i<inRow**2;i++){
     gridContainer.appendChild(square);
 
     square.addEventListener("mouseover",()=>{
-        square.style.backgroundColor="blue"
+        square.style.backgroundColor=color;
     });
 }
+
+//variables for color picker
+const colorPicker=document.querySelector("input");
+const colorOutput=document.querySelector("label");
+
+addEventListener("input",event=>{
+    colorOutput.innerText=colorPicker.value;
+    color=colorPicker.value;
+})
+
+
+
