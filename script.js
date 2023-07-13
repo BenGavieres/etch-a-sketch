d3c78f34df314890e810d221edfa457ae540c726
BenGavieres <benjamin.gavieres@gmail.com>
2023-07-12 21:57:56 -0400

    Added a color button and a corresponding boolean variable to enable color mode.
    Added functionality to color and erase buttons.
    
    Now updates the color whenever the color button is clicked.
    Fixed a bug where color would not update if the color picker
    was interacted with while not in color mode.

--- script.js
+++ script.js
@@ -4,33 +4,87 @@ GRID_WIDTH=parseInt(GRID_PIXELS,10);//removes "px" from end of GRID_PIXELS strin
 //grid width is 400 px
 
 let inRow=16;//can be changed by user
-let squareWidth=Math.floor(GRID_WIDTH/inRow);
+let squareWidth;
 let color="#000000"//can be changed by user
 
-for (let i=0;i<inRow**2;i++){
-    const square=document.createElement("div");
-    square.classList.add("square");
-     
-    square.style.width=`${squareWidth}.px`;
-    square.style.height=`${squareWidth}.px`;
-
-    gridContainer.appendChild(square);
-
-    square.addEventListener("mouseover",()=>{
-        square.style.backgroundColor=color;
-    });
-}
-
-//color picker
 const colorPicker=document.querySelector("input");
 const colorOutput=document.querySelector("label");
 
-colorOutput.innerText=colorPicker.value;
+const colorBtn=document.querySelector("button.color");
+const erase=document.querySelector("button.erase");
+const resize=document.querySelector("button.resize");
+const rainbow=document.querySelector("button.random-color");
 
+let isColorMode=true;
+let isEraseMode=false;
+
+//gen grid
+function generateGrid(){
+    squareWidth=Math.floor(GRID_WIDTH/inRow);
+    for (let i=0;i<inRow**2;i++){
+        const square=document.createElement("div");
+        square.classList.add("square");
+        
+        square.style.width=`${squareWidth}.px`;
+        square.style.height=`${squareWidth}.px`;
+
+        gridContainer.appendChild(square);
+
+        square.addEventListener("mouseover",()=>{
+            if (isColorMode) {
+                square.style.backgroundColor=color;
+            }
+            else if (isEraseMode){
+                square.style.backgroundColor="white";
+            }
+        });
+    }
+}
+
+//degen grid
+function degenerateGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
+
+//color picker
+colorOutput.innerText=colorPicker.value;
 addEventListener("input",event=>{
-    colorOutput.innerText=colorPicker.value;
-    color=colorPicker.value;
+    if (isColorMode){
+        colorOutput.innerText=colorPicker.value;
+        color=colorPicker.value;
+    }
 })
+
+//color mode
+colorBtn.addEventListener("click",()=>{
+    color=colorPicker.value;
+    isColorMode=true;
+    isEraseMode=false;
+    colorBtn.classList.add("selected");
+    erase.classList.remove("selected");
+})
+
+//erase
+erase.addEventListener("click",()=>{
+    isColorMode=false;
+    isEraseMode=true;
+    colorBtn.classList.remove("selected");
+    erase.classList.add("selected");
+});
+
+//resize
+resize.addEventListener("click",()=>{
+    inRow=prompt("Enter new grid width/height (1-100)");
+    console.log(inRow);
+    degenerateGrid();
+    generateGrid();
+})
+
+generateGrid();
+
+
 
 
 
